419416a4f61bfaeb12034240ff2d2c697fe1788d
Hosanna Bivan <hosabivan@gmail.com>
2024-04-01 16:51:15 +0100

    women menu 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showMenu(){
     let womenMenu = $("#dropdown-menu")
-    let fullPage =$("#fullpage")
+    let fullPage = $("#fullpage")
     womenMenu.fadeToggle(200)
     fullPage.toggleClass("dark-background");
 }
