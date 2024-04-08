93bf18053f071b5d128423ede661cf78a137db7d
Hosanna Bivan <hosabivan@gmail.com>
2024-04-08 12:37:03 +0100

    cart

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+let cartpage = $("#cart_page")
+let fullPage = $("#fullpage")
+
+
 function showMenu(){
     let womenMenu = $("#dropdown-menu")
     let fullPage = $("#fullpage")
     womenMenu.fadeToggle(200)
     fullPage.toggleClass("dark-background");
 }
+
+
+function showCartPage() {
+    cartpage.fadeIn(300)
+    fullPage.addClass("dark-background");
+    
+}
+
+function closeCartPage(){
+    cartpage.fadeOut(300)
+    fullPage.removeClass("dark-background");
+
+}
+
