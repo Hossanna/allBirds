c603befa25988c96a2ca99fdc66f92dc993c5ad4
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-04-11 13:20:31 +0100

    Handle dropdown clicks

--- main.js
+++ main.js
@@ -4,13 +4,40 @@
 //   });
 // });
 
-let dropdownContent = document.getElementsByClassName("dropdownContent3");
+// let dropdownContent = document.getElementsByClassName("dropdownContent3");
 
-dropdownContent.addEventListner("click", function () {
-  dropdownContent.classList.add("show");
-  dropdownContent.style.backgroundColor = "blue";
-  dropdownContent.style.color = "blue";
-  dropdownContent.style.fontSize = "50";
+// dropdownContent.addEventListner("click", function () {
+//   dropdownContent.classList.add("show");
+//   dropdownContent.style.backgroundColor = "blue";
+//   dropdownContent.style.color = "blue";
+//   dropdownContent.style.fontSize = "50";
+// });
+
+// let find = document.getElementsByClassName("find");
+// let showDiv = document.getElementsByClassName("storeDropdown");
+
+// function show (){
+//   find.classList.toggle("visible");
+// }
+
+$(document).ready(function () {
+  $(".find").click(function () {
+    $(".storeDropdown").toggle(".visible");
+  });
+
+  $(".dropdown1").click(function () {
+    $(".dropdownContent1").toggle(".visible");
+  });
+
+  $(".dropdown2").click(function () {
+    $(".dropdownContent2").toggle(".visible");
+  });
+
+  $(".dropdown3").click(function () {
+    $(".dropdownContent3").toggle(".visible");
+  });
+
+  $(".dropdown4").click(function () {
+    $(".dropdownContent4").toggle(".visible");
+  });
 });
-
-
