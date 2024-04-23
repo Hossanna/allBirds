1511ff5a7a47ac2a1e4dd569fb5d212d8902f3ea
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-04-23 13:26:34 +0100

    validate admin input fields

--- main.js
+++ main.js
@@ -18,4 +18,63 @@ $(document).ready(function () {
   $(".dropdown4").click(function () {
     $(".dropdownContent4").toggle(".visible");
   });
+
+  $("#register_button").click(function () {
+    let nameValidated = validateNotEmpty("name");
+    let lastnameValidated = validateNotEmpty("last_name");
+    let registrationemailValidated = validateNotEmpty("reg_email");
+    let registrationpasswordValidated = validateNotEmpty("regPassword");
+    let confirmpasswordValidated = validateNotEmpty("confirmPassword");
+    let registrationpasswordLengthValidated = validateLength("regPassword", 8);
+    let confirmpasswordLengthValidated = validateLength("confirmPassword", 8);
+
+    function validateNotEmpty(className) {
+      if (!$("#" + className).val()) {
+        $("#" + className).addClass("error");
+        return false;
+      } else {
+        $("#" + className).removeClass("error");
+        return true;
+      }
+    }
+
+    function validateLength(className, length) {
+      if ($("#" + className).val().length < length) {
+        $("#" + className).addClass("error");
+        alert(className + " must be at least " + length + "characters");
+        return false;
+      } else {
+        $("#" + className).removeClass("error");
+        return true;
+      }
+    }
+  });
+
+  
+  $("#login_button").click(function () {
+    let emailValidated = validateNotEmpty("email");
+    let passwordValidated = validateNotEmpty("password");
+    let passwordLengthValidated = validateLength("password", 8);
+
+    function validateNotEmpty(className) {
+      if (!$("#" + className).val()) {
+        $("#" + className).addClass("error");
+        return false;
+      } else {
+        $("#" + className).removeClass("error");
+        return true;
+      }
+    }
+
+    function validateLength(className, length) {
+      if ($("#" + className).val().length < length) {
+        $("#" + className).addClass("error");
+        alert(className + " must be at least " + length + "characters");
+        return false;
+      } else {
+        $("#" + className).removeClass("error");
+        return true;
+      }
+    }
+  });
 });
